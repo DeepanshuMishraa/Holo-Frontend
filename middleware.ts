9d7dde4fad5bf355fbc949fda6b746261f4bb2e8
Deepanshu <d4deepanshu723@gmail.com>
2025-03-11 22:41:44 +0530

    Fix middleware in prod

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
+import { betterFetch } from "@better-fetch/fetch";
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { authClient } from "./lib/auth-client";
+
+type Session = typeof authClient.$Infer.Session;
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
-  if (!sessionCookie) {
+  const { data: session } = await betterFetch<Session>(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/get-session`, {
+    baseURL: request.nextUrl.origin,
+    headers: {
+      cookie: request.headers.get("cookie") || "",
+    },
+  });
+
+  if (!session) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
   return NextResponse.next();
 }
 
